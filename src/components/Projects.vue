245df6e14ad901a75b56025f13b2564d54d404dd
Javi Penaloza <veralece@gmail.com>
2023-04-15 18:14:12 -0700

    updated projects data, css, and page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,33 +4,55 @@ import data from "../data/projects.json";
 <template>
     <section id="projects">
         <h1 class="text-gradient">Projects</h1>
-        <section class="project card" v-for="project in data.projects">
-            <h2 class="text-gradient"><a :href="project.url">{{ project.projectTitle }}</a></h2>
-            <section class="grid project-desc">
-                <p><strong>Problem: </strong>{{ project.problem }}</p>
-                <p><strong>Approach: </strong>{{ project.approach }}</p>
-                <p><strong>Solution: </strong>{{ project.solution }}</p>
-                <p><strong>Results: </strong>{{ project.results }}</p>
-                <p class="demo"><a :href="project.url">Link to site</a></p>
+        <section class="auto-grid">
+            <section class="project card" v-for="project in data.projects">
+                <h2 class="text-gradient"><a :href="project.url">{{ project.projectTitle }}</a></h2>
+                <a :href="project.url">
+                    <img class="project img" v-if="project.imgs !== ''" :src="`/imgs/${project.imgs}`"
+                        :alt="project.projectTitle">
+                </a>
+                <section class="grid project-desc">
+                    <p><strong>Problem: </strong>{{ project.problem }}</p>
+                    <p><strong>Approach: </strong>{{ project.approach }}</p>
+                    <p><strong>Solution: </strong>{{ project.solution }}</p>
+                    <p><strong>Results: </strong>{{ project.results }}</p>
+                    <p class="demo"><a :href="project.url">Link to site</a></p>
+                </section>
+                <h3>Technology utilized</h3>
+                <ul class="tech flex">
+                    <li v-for="tech in project.techStack">{{ tech }}</li>
+                </ul>
             </section>
-            <h3>Technology utilized</h3>
-            <ul class="tech flex">
-                <li v-for="tech in project.techStack">{{ tech }}</li>
-            </ul>
         </section>
     </section>
 </template>
 <style scoped>
+.auto-grid {
+    display: grid;
+    margin: 0 auto;
+    padding: 0 1em;
+    column-gap: 2em;
+    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+    max-width: 1600px;
+}
+
 .demo {
-    border: 1px solid var(--link-hover);
+    border: 2px solid var(--link-hover);
     border-radius: 5px;
     width: fit-content;
     place-self: center;
+    transition: box-shadow 150ms ease-in-out;
+    display: block;
+    box-shadow: 0 0 0 0 var(--text-color) inset;
 }
 
 .demo a {
     padding: 1em;
     display: block;
+}
+
+.demo:hover {
+    box-shadow: 0 0 0 100px var(--text-color) inset;
 }
 
 .project-desc.grid {
@@ -42,6 +64,19 @@ import data from "../data/projects.json";
 .project.card>h2 {
     font-size: 1.75rem;
     margin: .83em;
+}
+
+.project.img {
+    width: calc(100% - 2em);
+    margin: 0 1em;
+    border-radius: var(--card-border-size);
+    box-shadow: 0 0 10px 0 #333;
+    max-width: 600px;
+    transition: box-shadow 250ms ease-in-out;
+}
+
+.project.img:hover {
+    box-shadow: 0 0 50px 0px var(--link-hover);
 }
 
 .tech {
@@ -70,5 +105,11 @@ import data from "../data/projects.json";
 #projects {
     text-align: center;
     margin-top: 5em;
+}
+
+@media screen and (max-width: 425px) {
+    .auto-grid {
+        grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    }
 }
 </style>
